8880c6100ab5105a69a741691d6a869a1a9eb304
aranajhonny <jhonnyjosearana@gmail.com>
2018-05-15 14:44:58 -0400

    init repo

--- config.js
+++ config.js
@@ -1,4 +1,16 @@
 const { schema, imports, dependencies, environment, expressions, endpoints } = program;
 
 schema.type('Root')
- 
+  .computed('dolar', 'Dolar')
+
+schema.type('Dolar')
+  .field('USD', 'USD')
+  .field('USD', 'EUR')
+
+schema.type('USD')
+  .field('dolartoday', 'String')
+  .field('bitcoin', 'String')
+
+schema.type('EUR')
+  .field('dolartoday', 'String')
+
--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const { root } = program.refs
 
-export async function init() {
-  // Called when the program is run
+export const Root = {
+  async Dolar({ args }) {
+    const result = await axios.get('https://s3.amazonaws.com/dolartoday/data.json');
+    return result.data
+  },
 }
-
-export async function update() {
-  // Called when the program is updated from a previous version
-}
-
-export async function timer({ key }) {
-  // Called every time a timer fires
-}
-
