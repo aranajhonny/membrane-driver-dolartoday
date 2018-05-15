0a3ddb82c1467aaa992a64f7db9f9cc1c1324802
aranajhonny <jhonnyjosearana@gmail.com>
2018-05-15 14:47:08 -0400

    change name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { root } = program.refs
 
 export const Root = {
-  async Dolar({ args }) {
+  async dolar({ args }) {
     const result = await axios.get('https://s3.amazonaws.com/dolartoday/data.json');
     return result.data
   },
